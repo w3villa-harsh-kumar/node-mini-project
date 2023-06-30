6c3d7b155173192ded5398292bf77e04954e1b94
w3villa-harsh-kumar <harsh.kumar@w3villa.com>
2023-06-30 10:59:59 +0530

    Adding some npm package for ensuring security

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 require("express-async-errors");
+const { StatusCodes } = require("http-status-codes");
 
-// swagger
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocs = require("./swagger/swagger.json");
+// security
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const cors = require("cors");
 
 // database connection
 const connectMongoDB = require("./db/db.connect");
@@ -12,7 +14,12 @@ const connectMongoDB = require("./db/db.connect");
 const logger = require("./loggers");
 const { httpLogger, errorHttpLogger } = require("./loggers/httpLogger");
 
+// swagger
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocs = require("./swagger/swagger.json");
+
 // error handler
+const { AnonmyousError } = require("./errors");
 const notFoundMiddleware = require("./middlewares/not-found");
 const errorHandlerMiddleware = require("./middlewares/error-handler");
 
@@ -20,17 +27,35 @@ const errorHandlerMiddleware = require("./middlewares/error-handler");
 const userRoutes = require("./routes/users.routes");
 const taskRoutes = require("./routes/tasks.routes");
 
-const cors = require("cors");
+// Express App
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// express middleware
-app.use(
-    cors({
-        origin: process.env.CORS_ORIGIN,
-    })
-);
+// Middlewares
+const limiter = rateLimit({
+    windowMs: 15 * Number(process.env.RATE_LIMIT_WINDOW_MS),
+    max: Number(process.env.RATE_LIMIT_MAX),
+    standardHeaders: true,
+    legacyHeaders: false,
+    handler(req, res, next) {
+        logger.error(`Too many requests from ${req.ip} at ${req.originalUrl}`);
+        throw new AnonmyousError(
+            `Too many requests, please try again in ${Math.round(
+                this.windowMs / process.env.RATE_LIMIT_WINDOW_MS
+            )} minutes`,
+            StatusCodes.TOO_MANY_REQUESTS
+        );
+    },
+});
+const corsOptions = {
+    origin: process.env.CORS_ORIGIN,
+    optionsSuccessStatus: 200,
+};
+app.set("trust proxy", Number(process.env.TRUST_PROXY));
+app.use(limiter);
+app.use(cors(corsOptions));
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(httpLogger);
